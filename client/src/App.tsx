2e3c4f334eb56b5ebbf50c196e424bb65f5aba19
ohoon <0hoon5661@gmail.com>
2021-01-11 16:48:32 +0900

    Add default router settings

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Switch>
+      <Route
+        path="/"
+        exact
+        component={Home}
+      />
+      <Route
+        render={({ location }) => (
+          <div
+            style={{ padding: "30px" }}
+          >
+            <h3>
+              이 페이지는 존재하지 않습니다.
+            </h3>
+            <p>
+              path={location.pathname}
+            </p>
+          </div>
+        )}
+      />
+    </Switch>
   );
 }
 
