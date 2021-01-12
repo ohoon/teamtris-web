d3509692c68cdfaac26d02c5a3afed0ecb22d8f4
ohoon <0hoon5661@gmail.com>
2021-01-12 16:28:37 +0900

    Make express use ES Module System

--- server/app.js
+++ server/app.js
@@ -1,9 +1,8 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
 
-const indexRouter = require('./routes/index');
+import indexRouter from './routes/index.js';
 
 const app = express();
 
@@ -11,8 +10,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-module.exports = app;
+export default app;
--- server/bin/www.js
+++ server/bin/www.js
@@ -4,9 +4,10 @@
  * Module dependencies.
  */
 
-const app = require('../app');
-const debug = require('debug')('server:server');
-const http = require('http');
+import app from '../app.js';
+import debugLib from 'debug';
+import http from 'http';
+const debug = debugLib('server:server');
 
 /**
  * Get port from environment and store in Express.
