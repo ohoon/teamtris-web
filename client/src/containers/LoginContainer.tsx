6891beac551e349fe8b2a80557345a35e27cde93
ohoon <0hoon5661@gmail.com>
2021-01-19 21:01:24 +0900

    Fix me state is not reload before refresh page

--- client/src/containers/LoginContainer.tsx
+++ client/src/containers/LoginContainer.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import axios from 'axios';
 import { getAccessToken, LoginInputs } from '../api/auth';
+import { getMeThunk } from '../modules/users';
 import LoginForm from '../components/LoginForm';
 
 function LoginContainer() {
     const history = useHistory();
+    const dispatch = useDispatch();
     const onSubmit = async (input: LoginInputs) => {
         try {
             const result = await getAccessToken(input);
@@ -16,6 +19,7 @@ function LoginContainer() {
             const accessToken = result.data;
             localStorage.setItem('ACCESS_TOKEN', accessToken);
             axios.defaults.headers.authorization = `Bearer ${accessToken}`;
+            dispatch(getMeThunk());
             history.push('/');
         } catch (err) {
             if (err.error) {
