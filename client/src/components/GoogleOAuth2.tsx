ce4c9c275817092510b2d650ba8b3ec8027b9514
ohoon <0hoon5661@gmail.com>
2021-02-22 18:40:32 +0900

    Make user get jwt token after google login

--- client/src/components/GoogleOAuth2.tsx
+++ client/src/components/GoogleOAuth2.tsx
@@ -1,5 +1,9 @@
 import React, { useEffect } from 'react';
 import qs from 'qs';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { getMeThunk } from '../modules/users';
 import { getAccessTokenGoogle } from '../api/auth';
 
 interface GoogleOAuth2Props {
@@ -7,15 +11,37 @@ interface GoogleOAuth2Props {
 }
 
 function GoogleOAuth2({ location }: GoogleOAuth2Props) {
+    const dispatch = useDispatch();
+    const history = useHistory();
+
     const query = qs.parse(location.search, {
         ignoreQueryPrefix: true
     });
 
     useEffect(() => {
-        getAccessTokenGoogle({ code: query.code!.toString() });
+        (async () => {
+            try {
+                const result = await getAccessTokenGoogle({ code: query.code!.toString() });
+                if (!result.success) {
+                    throw result;
+                }
+    
+                const accessToken = result.data;
+                localStorage.setItem('ACCESS_TOKEN', accessToken);
+                axios.defaults.headers.authorization = `Bearer ${accessToken}`;
+                dispatch(getMeThunk());
+                history.push('/');
+            } catch (err) {
+                alert('로그인 실패');
+            }
+        })();
     });
 
-    return null;
+    return (
+        <div>
+            로그인중...
+        </div>
+    );
 }
 
 export default GoogleOAuth2;
