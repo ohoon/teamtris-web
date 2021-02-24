4f04f5bdcfe01745c6e18ad92c00b7d9214803f0
ohoon <0hoon5661@gmail.com>
2021-02-24 17:03:09 +0900

    Modify room item UI

--- client/src/components/RoomItem.tsx
+++ client/src/components/RoomItem.tsx
@@ -1,20 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
+import { BsFillLockFill } from 'react-icons/bs';
 import { WaitingPlayer } from '../../../server/src/socket/users';
 
 const RoomItemBlock = styled.div`
     display: flex;
-    position: relative;
+    align-items: center;
     margin: 4px;
-    padding: 16px;
+    padding: 16px 20px 16px 20px;
     border: 1px solid #D8D8D8;
     border-radius: 16px;
     background: #D5D9DE;
     font-size: 16px;
     cursor: pointer;
 
+    .roomId {
+        font-size: 25px;
+        font-weight: bold;
+        padding-right: 4%;
+        border-right: 1px solid #747E87;
+    }
+
+    .people {
+        position: absolute;
+        right: 6%;
+        top: 15%;
+        font-size: 13px;
+    }
+    
     &:hover {
         background: #E0E4E8;
+    }
+`;
+
+const RoomInfoBlock = styled.div`
+    padding-left: 4%;
+    border-left: 1px soild;
+
+    .title {
+        display: flex;
+        align-items: center;
+        font-weight: bold;
+    }
+
+    .mode {
+        font-size: 14px;
     }
 `;
 
@@ -47,12 +77,30 @@ function RoomItem({ roomId, title, password, current, max, mode, onJoinRoom }: R
         <RoomItemBlock
             onClick={handleJoinRoom}
         >
-            [{roomId}]
-            [{mode}]
-            {title}
-            {password && '(private)'}
-            <br />
-            {current} / {max}
+            <div
+                className="roomId"
+            >
+                {roomId}
+            </div>
+            <RoomInfoBlock>
+                <div
+                    className="title"
+                >
+                    {title}
+                    &nbsp;
+                    {password && <BsFillLockFill />}
+                </div>
+                <div
+                    className="mode"
+                >
+                    {mode === 'single' ? '개인전' : '팀전'}
+                </div>
+            </RoomInfoBlock>
+            <div
+                className="people"
+            >
+                {current} / {max}
+            </div>
         </RoomItemBlock>
     );
 }
